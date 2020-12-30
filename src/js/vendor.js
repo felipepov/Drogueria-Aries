9385bff6cad4f677b118e0f55a63707fe84ff426
fpovina2004 <57961919+fpovina2004@users.noreply.github.com>
2020-12-30 17:22:16 -0300

    Initial commit

---
+++ src/js/vendor.js
@@ -0,0 +1,4 @@
+// You can specify which plugins you need
+// import { Tooltip, Toast, Popover } from 'bootstrap';
+// Alternatively, if you only need just a few of our plugins, you may import plugins individually as needed:
+// import Alert from 'bootstrap/js/dist/alert'
